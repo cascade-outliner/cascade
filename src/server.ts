a0208acb3ac620f709911f217c87d99f7155b8a4
Patrick <contact@patrickroelofs.com>
2026-06-26 22:10:06 +0200

    fix: remove unused imports and set FastResponse in global scope in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,3 @@
-import {
-	createStartHandler,
-	defaultStreamHandler,
-} from "@tanstack/react-start/server";
 import { FastResponse } from "srvx";
-
-export default { fetch: createStartHandler(defaultStreamHandler) };
 
 globalThis.Response = FastResponse;
