5bf58a0f15f6e88375566ff6a94a9d3c39cd47d9
Patrick <contact@patrickroelofs.com>
2026-06-26 20:58:56 +0200

    fix: implement server start handler in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,9 @@
+import {
+	createStartHandler,
+	defaultStreamHandler,
+} from "@tanstack/react-start/server";
 import { FastResponse } from "srvx";
 
 globalThis.Response = FastResponse;
+
+export default { fetch: createStartHandler(defaultStreamHandler) };
