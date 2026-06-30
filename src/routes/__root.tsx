303d272c98348aa0624fbabd72dae66ec838a4ad
Patrick <contact@patrickroelofs.com>
2026-06-30 20:07:10 +0200

    feat: update body styling with background color and text color for improved readability

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -45,7 +45,7 @@ function RootDocument({ children }: { children: React.ReactNode }) {
 			<head>
 				<HeadContent />
 			</head>
-			<body>
+			<body className="bg-ginger text-dark-grey">
 				{children}
 				<TanStackDevtools
 					config={{
