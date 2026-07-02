50b2223acd17b537b0e9cf74de70b414e0da04f4
Patrick <contact@patrickroelofs.com>
2026-07-02 20:41:12 +0200

    feat: add path aliasing in Vite configuration for improved module resolution

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from "node:path";
 import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
 import { devtools } from "@tanstack/devtools-vite";
@@ -9,6 +10,12 @@ import { defineConfig } from "vite";
 export default defineConfig({
 	resolve: {
 		tsconfigPaths: true,
+		alias: [
+			{
+				find: /^#\/(.*)/,
+				replacement: path.resolve(import.meta.dirname, "src/$1"),
+			},
+		],
 	},
 	plugins: [
 		devtools(),
