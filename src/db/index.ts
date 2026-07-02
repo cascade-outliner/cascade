bb2444cee6857b85eeca453e044bea06f2976078
Patrick <contact@patrickroelofs.com>
2026-07-02 17:52:27 +0200

    feat: simplify database client initialization by using env module for DATABASE_URL

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,12 +1,7 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
+import { env } from "#/env";
 import * as schema from "./schema";
 
-const databaseUrl = process.env.DATABASE_URL;
-
-if (!databaseUrl) {
-	throw new Error("DATABASE_URL environment variable is not set.");
-}
-
-const client = postgres(databaseUrl);
+const client = postgres(env.DATABASE_URL);
 export const db = drizzle(client, { schema });
