b81cdaafc4d2e43c3a57c6ce4bb7826cedae0478
Patrick Roelofs <contact@patrickroelofs.com>
2026-08-12 21:23:07 +0200

    Delete apps/api directory (#716)

--- apps/api/src/logger/types/log-entry.ts
+++
@@ -1,9 +0,0 @@
-import type { LogLevel } from "@nestjs/common";
-
-export interface LogEntry {
-	level: LogLevel;
-	message: string;
-	context?: string;
-	trace?: string;
-	timestamp: Date;
-}
--- apps/api/src/modules/nodes/dto/metadata.node.dto.ts
+++
@@ -1,8 +0,0 @@
-import { createZodDto } from "nestjs-zod";
-import { z } from "zod";
-
-export const metadataNodeSchema = z.object({
-	expanded: z.boolean(),
-});
-
-export class MetadataNodeDto extends createZodDto(metadataNodeSchema) {}
