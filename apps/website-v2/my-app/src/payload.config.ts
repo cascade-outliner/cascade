e188ed1a919db35c140be5b402600cc72934737f
Patrick Roelofs <contact@patrickroelofs.com>
2026-08-18 09:04:33 +0200

    initial setup v2 website with payload

---
+++ apps/website-v2/my-app/src/payload.config.ts
@@ -0,0 +1,41 @@
+import { postgresAdapter } from "@payloadcms/db-postgres";
+import { mcpPlugin } from "@payloadcms/plugin-mcp";
+import { lexicalEditor } from "@payloadcms/richtext-lexical";
+import path from "path";
+import { buildConfig } from "payload";
+import sharp from "sharp";
+import { fileURLToPath } from "url";
+import { Folders } from "./collections/Folders";
+import { Media } from "./collections/Media";
+import { Tags } from "./collections/Tags";
+import { Users } from "./collections/Users";
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
+export default buildConfig({
+	admin: {
+		user: Users.slug,
+		importMap: {
+			baseDir: path.resolve(dirname),
+		},
+	},
+	collections: [Users, Media, Folders, Tags],
+	editor: lexicalEditor(),
+	secret: process.env.PAYLOAD_SECRET || "",
+	typescript: {
+		outputFile: path.resolve(dirname, "payload-types.ts"),
+	},
+	db: postgresAdapter({
+		pool: {
+			connectionString: process.env.DATABASE_URL || "",
+		},
+	}),
+	sharp,
+	localization: {
+		locales: ["en"],
+		fallback: true,
+		defaultLocale: "en",
+	},
+	plugins: [mcpPlugin({})],
+});
