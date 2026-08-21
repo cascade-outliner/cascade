011c8f349d7e854c11dd9c21cf683e4e4290f5cc
Patrick Roelofs <contact@patrickroelofs.com>
2026-08-21 19:34:11 +0200

    feat: add migration for authors, blogs, media, and pages schema with related types and constraints

---
+++ apps/website/src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260821_173330 from './20260821_173330';
+
+export const migrations = [
+  {
+    up: migration_20260821_173330.up,
+    down: migration_20260821_173330.down,
+    name: '20260821_173330'
+  },
+];
